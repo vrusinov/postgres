5f46338ffb481ed4883bef02bd656fc1eb62717a
Vladimir Rusinov <vrusinov@google.com>
2017-01-12 15:44:32 +0000

    Rename pg_current_xlog_flush_location alias.

--- src/include/catalog/pg_proc.h
+++ src/include/catalog/pg_proc.h
@@ -3155,24 +3155,14 @@ DATA(insert OID = 3098 ( pg_create_restore_point	PGNSP PGUID 12 1 0 0 0 f f f f 
 DESCR("create a named restore point");
 DATA(insert OID = 3800 ( pg_current_wal_location         PGNSP PGUID 12 1 0 0 0 f f f f t f v s 0 0 3220 "" _null_ _null_ _null_ _null_ _null_ pg_current_wal_location _null_ _null_ _null_ ));
 DESCR("current WAL write location");
-DATA(insert OID = 2849 ( pg_current_xlog_location        PGNSP PGUID 12 1 0 0 0 f f f f t f v s 0 0 3220 "" _null_ _null_ _null_ _null_ _null_ pg_current_wal_location _null_ _null_ _null_ ));
-DESCR("historical alias for pg_current_xlog_location");
 DATA(insert OID = 3801 ( pg_current_wal_insert_location  PGNSP PGUID 12 1 0 0 0 f f f f t f v s 0 0 3220 "" _null_ _null_ _null_ _null_ _null_ pg_current_wal_insert_location _null_ _null_ _null_ ));
 DESCR("current WAL insert location");
-DATA(insert OID = 2852 ( pg_current_xlog_insert_location PGNSP PGUID 12 1 0 0 0 f f f f t f v s 0 0 3220 "" _null_ _null_ _null_ _null_ _null_ pg_current_wal_insert_location _null_ _null_ _null_ ));
-DESCR("historical alias for pg_current_wal_insert_location");
 DATA(insert OID = 6015 ( pg_current_wal_flush_location   PGNSP PGUID 12 1 0 0 0 f f f f t f v s 0 0 3220 "" _null_ _null_ _null_ _null_ _null_ pg_current_wal_flush_location _null_ _null_ _null_ ));
 DESCR("current WAL flush location");
-DATA(insert OID = 3330 ( pg_current_xlog_flush_location  PGNSP PGUID 12 1 0 0 0 f f f f t f v s 0 0 3220 "" _null_ _null_ _null_ _null_ _null_ pg_current_wal_flush_location _null_ _null_ _null_ ));
-DESCR("historical alias for pg_current_wal_flush_location");
 DATA(insert OID = 3452 ( pg_wal_file_name_offset	       PGNSP PGUID 12 1 0 0 0 f f f f t f i s 1 0 2249 "3220" "{3220,25,23}" "{i,o,o}" "{wal_location,file_name,file_offset}" _null_ _null_ pg_wal_file_name_offset _null_ _null_ _null_ ));
 DESCR("WAL filename and byte offset, given an WAL location");
-DATA(insert OID = 2850 ( pg_xlogfile_name_offset	       PGNSP PGUID 12 1 0 0 0 f f f f t f i s 1 0 2249 "3220" "{3220,25,23}" "{i,o,o}" "{wal_location,file_name,file_offset}" _null_ _null_ pg_wal_file_name_offset _null_ _null_ _null_ ));
-DESCR("historical alias for pg_wal_file_name_offset");
 DATA(insert OID = 3453 ( pg_wal_file_name			           PGNSP PGUID 12 1 0 0 0 f f f f t f i s 1 0 25   "3220" _null_ _null_ _null_ _null_ _null_ pg_wal_file_name _null_ _null_ _null_ ));
-DESCR("xlog filename, given an xlog location");
-DATA(insert OID = 2851 ( pg_xlogfile_name			           PGNSP PGUID 12 1 0 0 0 f f f f t f i s 1 0 25   "3220" _null_ _null_ _null_ _null_ _null_ pg_wal_file_name _null_ _null_ _null_ ));
-DESCR("historical alias for pg_wal_file_name");
+DESCR("WAL filename, given a WAL location");
 
 DATA(insert OID = 3445 ( pg_wal_location_diff		PGNSP PGUID 12 1 0 0 0 f f f f t f i s 2 0 1700 "3220 3220" _null_ _null_ _null_ _null_ _null_ pg_wal_location_diff _null_ _null_ _null_ ));
 DESCR("difference in bytes, given two wal (aka xlog) locations");
